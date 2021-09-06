4f68d5bc66f275570fb73638eb923e54b3fe3bc2
Chloe Lunn <chloel@1602group.com>
2021-09-06 12:57:01 +0100

    comment on SAMD21 mem space

--- xmem.h
+++ xmem.h
@@ -8,7 +8,6 @@
  *  This work is licensed under a Creative Commons Attribution-ShareAlike 3.0 Unported License.
  */
 
-
 #ifndef __89089DA1_BAAC_497C_8E1FFEF0911A6844
 #define __89089DA1_BAAC_497C_8E1FFEF0911A6844
 
@@ -17,52 +16,52 @@
 
 namespace xmem {
 
-	/*
+/*
 	 * Pointers to the start and end of memory
 	 */
 
-#define XMEM_START ((void *)0x2200)
-#define XMEM_END ((void *)0xFFFF)
+#define XMEM_START ((void*)0x2200)  // 0x20000000 on SAMD21
+#define XMEM_END   ((void*)0xFFFF)  // 0x3FFFFFFF on SAMD21
 
-	/*
+/*
 	 * State variables used by the heap
 	 */
 
-	struct heapState {
-			char *__malloc_heap_start;
-			char *__malloc_heap_end;
-			void *__brkval;
-			void *__flp;
-	};
+struct heapState {
+    char* __malloc_heap_start;
+    char* __malloc_heap_end;
+    void* __brkval;
+    void* __flp;
+};
 
-	/*
+/*
 	 * Results of a self-test run
 	 */
 
-	struct SelfTestResults {
-			bool succeeded;
-			volatile uint8_t *failedAddress;
-			uint8_t failedBank;
-	};
+struct SelfTestResults {
+    bool succeeded;
+    volatile uint8_t* failedAddress;
+    uint8_t failedBank;
+};
 
-	/*
+/*
 	 * Prototypes for the management functions
 	 */
 
-	void begin(bool heapInXmem_);
-	void setMemoryBank(uint8_t bank_,bool switchHeap_=true);
-	SelfTestResults selfTest();
-	void saveHeap(uint8_t bank_);
-	void restoreHeap(uint8_t bank_);
-}
+void begin(bool heapInXmem_);
+void setMemoryBank(uint8_t bank_, bool switchHeap_ = true);
+SelfTestResults selfTest();
+void saveHeap(uint8_t bank_);
+void restoreHeap(uint8_t bank_);
+}  // namespace xmem
 
 /*
  * References to the private heap variables
  */
 
 extern "C" {
-	extern void *__flp;
-	extern void *__brkval;
+    extern void* __flp;
+    extern void* __brkval;
 }
 
 #endif
