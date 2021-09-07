eb0b7852ced2506885d2ca2b82d145c27e94f0d1
Chloe Lunn <chloetlunn@gmail.com>
2021-09-07 19:17:17 +0100

    start of front panel module

---
+++ ky11.cpp
@@ -0,0 +1,22 @@
+#include "ky11.h"
+
+namespace ky11 {
+uint16_t read8(uint32_t addr)
+{
+    // read front panel switches
+    return 0;
+}
+uint16_t read16(uint32_t addr)
+{
+    // read front panel switches
+    return 0;
+}
+void write8(uint32_t a, uint16_t v)
+{
+    // write to a front panel here
+}
+void write16(uint32_t a, uint16_t v)
+{
+    // write to a front panel here
+}
+}  // namespace ky11
