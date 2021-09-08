d2574338fccddea2f1a4b26e2df0de78189533a9
Chloe Lunn <chloel@1602group.com>
2021-09-08 10:06:53 +0100

    comment

--- ms11.h
+++ ms11.h
@@ -1,4 +1,4 @@
-#define MAX_UNIBUS_MEM (248 * 1024)  // 248K = 0760000 -- anything above this is mem-mapped IO
+#define MAX_UNIBUS_MEM (248 * 1024)  // 248Ki words = 0760000 -- anything above this is mem-mapped IO
 
 namespace ms11 {
 
