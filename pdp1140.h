76b481353e47610433b51b02629e3023bf67a803
Chloe Lunn <chloel@1602group.com>
2021-09-08 13:42:31 +0100

    device addresses

--- pdp1140.h
+++ pdp1140.h
@@ -16,6 +16,9 @@
  * Table of devices:
  * =================
  * 
+ * Items marked 'Y' are fully implemented
+ * Items marked 'P' are partially implemented
+ * 
  * Num:     I:  Description:
  * ---------------------------------------------------------------------------- 
  * 
@@ -27,7 +30,7 @@
  * KJ11     P   Stack Limit Register
  * KL11     Y   Main TTY Interface
  * KM11         Maintanance Device
- * KT11     Y   Memory Management Unit (MMU)
+ * KT11     P   Memory Management Unit (MMU)
  * KW11     Y   Line Time Clock (P revision is also RTC)
  * KY11         Developer/Diagnostics Console (front panel)
  * FP11         Floating Point Coprocessor
@@ -43,6 +46,7 @@
  * DL11         Serial (async) Line Controller
  * DM11         Serial (async) Line Controller
  * DQ11         Serial (NPR sync) Line Controller  
+ * DR11         Parallel Controller
  * DS11         Serial (sync) Line Controller 
  * DT11         Bus Switch
  * DU11         Serial (sync) Line Controller 
@@ -72,50 +76,61 @@
  * 
 */
 
+// Vectors & Addresses from DEC PDP-11/40 Processor Handbook Appendix B (1972)
+
 // Interrupt Vectors:
 enum
 {
-    INTRFU = 0000,     // Reserved
-    INTBUS = 0004,     // Bus timeout
-    INTINVAL = 0010,   // Reserved Intruction
-    INTDEBUG = 0014,   // Debugging trap
-    INTIOT = 0020,     // IOT Trap
-    INTPF = 0024,      // Power Fail
-    INTEMT = 0030,     // EMT
-    INTTRP = 0034,     // "TRAP"
-    INTSS0 = 0040,     // System Software 0
-    INTSS1 = 0044,     // System Software 1
-    INTSS2 = 0050,     // System Software 2
-    INTSS3 = 0054,     // System Software 3
-    INTTTYIN = 0060,   // TTY In
-    INTTTYOUT = 0064,  // TTY Out
-    INTPCRD = 0070,    // PC11 Reader
-    INTPCPU = 0074,    // PC11 Puncher
-    INTCLOCK = 0100,   // Line CLock
-    INTRTC = 0104,     // Programmer RTC
-    INTXYPLOT = 0120,  // XY Plotter
-    INTDR = 0124,      // DR11B parallel interface
-    INTADO1 = 0130,    //
-    INTAFC = 0134,     //
-    INTAASCP = 0140,   //
-    INTAALAMP = 0144,  //
-    INTUR0 = 0170,     //
-    INTUR1 = 0174,     //
-    INTLP = 0200,      // Line Printer
-    INTRF = 0204,      // RF11 Disk Control
-    INTRC 0210,        // RC11 Disk Control
-    INTTC11 = 0214,    // TC11 Disk Control
-    INTRK = 0220,      // RK11 Disk Control
-    INTTM = 0224,      // TM11 Disk Control
-    INTCR = 0230,      // CR11 Disk Control
-    INTUDC = 0234,     //
-    INT45PIRQ = 0240,  // 11/45 PIRQ
-    INTFPUERR = 0244,  // FPU Error
-    INTFAULT = 0250,   //
-    INTRP = 0254,      // RP Disk Control
+    INTRFU = 0000,       // Reserved
+    INTBUS = 0004,       // Bus timeout
+    INTINVAL = 0010,     // Reserved Intruction
+    INTDEBUG = 0014,     // Debugging trap
+    INTIOT = 0020,       // IOT Trap
+    INTPF = 0024,        // Power Fail
+    INTEMT = 0030,       // EMT
+    INTTRP = 0034,       // "TRAP"
+    INTSS0 = 0040,       // System Software 0
+    INTSS1 = 0044,       // System Software 1
+    INTSS2 = 0050,       // System Software 2
+    INTSS3 = 0054,       // System Software 3
+    INTTTYIN = 0060,     // TTY In
+    INTTTYOUT = 0064,    // TTY Out
+    INTPCRD = 0070,      // PC11 Reader
+    INTPCPU = 0074,      // PC11 Puncher
+    INTCLOCK = 0100,     // Line CLock
+    INTRTC = 0104,       // Programmer RTC
+    INTXYP = 0120,       // XY Plotter
+    INTDR = 0124,        // DR11B parallel interface
+    INTADO = 0130,       //
+    INTAFC = 0134,       //
+    INTAAS = 0140,       // AA11 Scope
+    INTAAL = 0144,       // AA11 Light
+    INTUR0 = 0170,       // User Reserved
+    INTUR1 = 0174,       // User Reserved
+    INTLP = 0200,        // Line Printer
+    INTRF = 0204,        // RF11 Disk Control
+    INTRC = 0210,        // RC11 Disk Control
+    INTTC = 0214,        // TC11 Disk Control
+    INTRK = 0220,        // RK11 Disk Control
+    INTTM = 0224,        // TM11 Disk Control
+    INTCR = 0230,        // CR11 Disk Control
+    INTUDC = 0234,       //
+    INTPIRQ1145 = 0240,  // 11/45 PIRQ
+    INTFPUERR = 0244,    // FPU Error
+    INTFAULT = 0250,     // General Fault (not in DEC's manual....?)
+    INTRP = 0254,        // RP Disk Control
 };
 
 // Device Addresses:
-#define BUS_CPU_STAT  0777776  // CPU Status
-#define BUS_STACK_LIM 0777774  // Stack Limit Register
-#define BUS_MEMORY    0760000  // Main Memory (0->0760000)
+namespace devaddr {
+enum
+{
+    CPU_STAT = 0777776,   // CPU Status
+    STACK_LIM = 0777774,  // Stack Limit Register
+    PIRQ_1145 = 0777772,  // 11/45 PIRQ Register
+    // 0777716 to 0777700,// CPU Registers
+    // 0777676 to 0777600 // 11/45 Segmentation Registers
+    // 0777656 to 0777600 // MX11 Memory Extention
+    MEMORY = 0760000,  // Main Memory (0->0760000)
+}
+};  // namespace devaddr
