3f2780cbdf11b1563a8e245e72ec22002a867d79
Chloe Lunn <chloetlunn@gmail.com>
2021-09-29 21:22:14 +0100

    mask on serial receive

--- kl11.cpp
+++ kl11.cpp
@@ -85,7 +85,7 @@ void poll()
         char c = Serial.read();
         if (FIRST_LF_BREAKS && (c == '\n' || c == '\r'))
             procNS::trapped = true;
-        addchar(c);
+        addchar(c & 0x7F);
     }
 
     if ((TPS & 0x80) == 0)
